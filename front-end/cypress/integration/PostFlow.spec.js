285aa808de31eb56d9c9ba4c9090ac1408bfcfb1
renaatosalgado <renaato.salgado@gmail.com>
2022-05-05 09:19:36 -0300

    feat: e2e testing - voting up and down

--- front-end/cypress/integration/PostFlow.spec.js
+++ front-end/cypress/integration/PostFlow.spec.js
@@ -3,6 +3,9 @@
 import { faker } from "@faker-js/faker";
 
 describe("Posting new song and voting up/down", () => {
+  beforeEach(() => {
+    cy.request("POST", "http://localhost:5000/resetDatabase");
+  });
   it("should correctly post a new song", () => {
     cy.visit("http://localhost:3000/");
 
@@ -16,9 +19,18 @@ describe("Posting new song and voting up/down", () => {
     cy.intercept("POST", "/recommendations").as("postRecommendation");
 
     cy.get("button[type=button]").click();
-
     cy.wait("@postRecommendation");
-
     cy.contains(name).should("be.visible");
+
+    cy.get('.upVote').click();
+    cy.get('.upVote').click();
+    cy.get('.score').should("have.text", "2")
+
+    cy.get('.downVote').click();
+    cy.get('.downVote').click();
+    cy.get('.downVote').click();
+    cy.get('.score').should("have.text", "-1")
+
+
   });
 });
