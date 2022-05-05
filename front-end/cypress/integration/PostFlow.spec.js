9d75b22c569a5b7e8d318365dafe8207a6fbdf3b
renaatosalgado <renaato.salgado@gmail.com>
2022-05-05 09:50:42 -0300

    feat: e2e testing - visiting pages /top and /random

--- front-end/cypress/integration/PostFlow.spec.js
+++ front-end/cypress/integration/PostFlow.spec.js
@@ -6,7 +6,7 @@ describe("Posting new song and voting up/down", () => {
   beforeEach(() => {
     cy.request("POST", "http://localhost:5000/resetDatabase");
   });
-  it("should correctly post a new song", () => {
+  it("should correctly post a new song, vote and remove by low score", () => {
     cy.visit("http://localhost:3000/");
 
     const name = faker.lorem.words(2);
@@ -22,15 +22,31 @@ describe("Posting new song and voting up/down", () => {
     cy.wait("@postRecommendation");
     cy.contains(name).should("be.visible");
 
-    cy.get('.upVote').click();
-    cy.get('.upVote').click();
-    cy.get('.score').should("have.text", "2")
+    cy.get(".upVote").click();
+    cy.get(".upVote").click();
+    cy.get(".upVote").click();
+    cy.get(".score").should("have.text", "3");
 
-    cy.get('.downVote').click();
-    cy.get('.downVote').click();
-    cy.get('.downVote').click();
-    cy.get('.score').should("have.text", "-1")
+    cy.get(".downVote").click();
+    cy.get(".downVote").click();
+    cy.get(".score").should("have.text", "1");
+  });
+});
 
+describe("Navigate to pages 'top' and 'random'", () => {
+  it("should visit the pages correctly", () => {
+    cy.visit("http://localhost:3000/");
 
+    cy.intercept("GET", "/recommendations/top/*").as("getTop");
+    cy.contains("Top").click();
+    cy.wait("@getTop");
+
+    cy.url().should("equal", "http://localhost:3000/top");
+
+    cy.intercept("GET", "/recommendations/random").as("getRandom");
+    cy.contains("Random").click();
+    cy.wait("@getRandom");
+
+    cy.url().should("equal", "http://localhost:3000/random");
   });
 });
