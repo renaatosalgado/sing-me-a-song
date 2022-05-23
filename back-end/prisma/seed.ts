864d9d464d0981b6e584561c32e4c96ec0f411b2
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-05-23 05:09:03 -0300

    Preparando revisão de código

--- back-end/prisma/seed.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../src/database.js";
-import { CreateRecommendationData } from "../src/services/recommendationsService.js";
-
-async function createRecommendation(recommendation: CreateRecommendationData) {
-  await prisma.recommendation.create({
-    data: recommendation,
-  });
-}
--- back-end/tests/factories/singleRecommendationFactory.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../../src/database.js";
-import recommendationBodyFactory from "./recommendationBodyFactory.js";
-
-export default async function singleRecommendationFactory() {
-  await prisma.recommendation.create({
-    data: recommendationBodyFactory(),
-  });
-}
